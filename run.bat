da123fe25eeb03a2efb5bf130121e56e86efd698
PaysanBarbare <bastien.rouches@gmail.com>
2021-06-19 23:16:35 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,16 @@
 @echo off
-
+if not "%minimized%"=="" goto :minimized
+set minimized=true
+start /min cmd /C "%~dpnx0"
+goto :EOF
+:minimized
+pushd %~dp0
 IF %PROCESSOR_ARCHITECTURE% == x86 (IF NOT DEFINED PROCESSOR_ARCHITEW6432 goto bit32)
 goto bit64
 :bit32
-start "" AutoIt3.exe BAO.a3x
-goto cont
+AutoIt3.exe BAO.a3x
+goto :end
 :bit64
-start "" AutoIt3_x64.exe BAO_x64.a3x
+AutoIt3_x64.exe BAO_x64.a3x
 :end
+popd
